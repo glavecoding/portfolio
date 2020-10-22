d2963cde8c2af8c68ff468655531754a52e8e7cf
glave <glavecoding@gmail.com>
2020-10-21 20:21:06 -0700

    Structure Edit 10/21/2020

---
+++ final/src/views/Open.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Opening">
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+   
+  },
+  mounted() {
+    this.router.push({path:'/home'})
+  },
+}
+</script>
